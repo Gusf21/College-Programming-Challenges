32a4e46d7132ddae6a646ba7d29d71f96eaf7cec
Fergus Fowler <fergusfowler@gmail.com>
2023-09-18 14:20:31 +0100

    initial commit

---
+++ Factorials/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace Program{
+
+    class Program{
+
+        static BigInteger getFactorial(BigInteger num){
+            if (num != 0){
+                return num * getFactorial(num - 1);
+            } else {
+                return (BigInteger)1;
+            }
+        }
+ 
+        static void Main(string[] args){
+            BigInteger num;
+            do{
+                Console.WriteLine("Enter Number");                
+            } while (!BigInteger.TryParse(Console.ReadLine(), out num));
+            BigInteger factorial = getFactorial(num);
+            Console.WriteLine($"Factorial of {num} is {factorial}");
+        }
+    }
+}
---
+++ fibonnaci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace testing {
+    
+    class Program {
+    
+        static void Main(string[] args) {
+            List<int> sequence = new List<int>{};
+            sequence.Add(0);
+            sequence.Add(1);
+            for (int i = 0; i < 30; i++) {
+                int len = sequence.Count();
+                sequence.Add(sequence[len-1] + sequence[len -2]);
+            }
+            foreach (int num in sequence) {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
