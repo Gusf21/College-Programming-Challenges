1e50002db05c5f305218ecaf484d343036c2b4ea
Fergus Fowler <fergusfowler@gmail.com>
2023-09-27 14:11:43 +0100

    Made pascal rows work

---
+++ Pascal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program{
+    class Program{
+
+        static int getNum(int row, int column){
+            //Console.WriteLine("Recursing");
+            //Console.WriteLine($"row = {row}, column = {column}");
+            if (column == 0 || row == column){
+                return 1;
+            } else {
+                return getNum(row-1, column-1) + getNum(row-1, column);
+            }
+        }
+
+        static List<int> getRow(int rowNum){
+            List<int> row = new List<int>();
+            for(int i = 0; i < rowNum+1; i++) {
+                int temp = getNum(rowNum, i); 
+                row.Add(temp);
+                //Console.WriteLine(temp);
+            }
+            return row;
+        }
+
+        static void Main(string[] args){
+            List<int> row = getRow(20);
+            foreach (int rowVal in row){
+                Console.WriteLine(rowVal);
+            }
+            Console.ReadLine();
+        }
+    }
+}
